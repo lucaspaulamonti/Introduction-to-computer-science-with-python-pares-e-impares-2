250400a706e0c9ec6cbb032f30a7f0ebd2f3ffab
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 14:02:31 -0300

    Create pares-e-impares-2.py

---
+++ pares-e-impares-2.py
@@ -0,0 +1,8 @@
+# Faça um programa em Python que identifica se um número é par ou ímpar.
+numero=(int(input("Insira um número inteiro: "))%2)
+if numero == 0:
+    print("par")
+else:
+    print("ímpar")
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
